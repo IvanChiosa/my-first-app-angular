88588448258af64be9f563d34e0cd6ea4827db54
Ivan <i85chiosa@gmail.com>
2023-03-08 15:19:11 +0100

    Angula

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ my-first-app/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    templateUrl: './success-alert.component.html',
+    styles: [
+        `p 
+        {
+            padding: 20px;
+            background-color: blue;
+            border: 1px solid red;
+        }
+        `
+    ]
+})
+
+export class SuccessAlertComponent {
+
+}
+
